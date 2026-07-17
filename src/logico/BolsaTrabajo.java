ca6ed771999cc0de6aa8d2b5d2df8e21f2449016
José Rafael <joserafaeltineog@gmail.com>
2026-07-17 17:07:54 -0400

    Actualizacion de la clase BolsaTrabajo.java. Correcion de errores e integracion de nuevas funcionalidades.

--- src/logico/BolsaTrabajo.java
+++ src/logico/BolsaTrabajo.java
@@ -3,11 +3,11 @@ package logico;
 import java.util.ArrayList;
 
 public class BolsaTrabajo {
-	public static int generadorIdPersona = 0;
-	public static int generadorIdSolicitud = 0;
-	public static int generadorIdOferta = 0;
-	public static int generadorIdInstitucion = 0;
-	public static int generadorIdUsuario = 0;
+	public static int generadorIdPersona = 1;
+	public static int generadorIdSolicitud = 1;
+	public static int generadorIdOferta = 1;
+	public static int generadorIdInstitucion = 1;
+	public static int generadorIdUsuario = 1;
 	private ArrayList<Persona> personas;
 	private ArrayList<Solicitud> solicitudes;
 	private ArrayList<Oferta> ofertas;
@@ -20,6 +20,7 @@ public class BolsaTrabajo {
 		this.solicitudes = new ArrayList<Solicitud>();
 		this.ofertas = new ArrayList<Oferta>();
 		this.instituciones = new ArrayList<Institucion>();
+		this.usuarios = new ArrayList<Usuario>();
 	}
 	
 	
@@ -90,4 +91,62 @@ public class BolsaTrabajo {
 		this.usuarios = usuarios;
 	}
 	
+	// Managment:
+	public String generarIdOferta() {
+		return "OFF-" + generadorIdOferta;  
+	}
+	
+	public String generarIdPersona() {
+		return "PER-" + generadorIdPersona; 
+	}
+	
+	public String generarIdUsuario() {
+		return "USU-" + generadorIdUsuario;
+	}
+	
+	public String generarIdInstitucion() {
+		return "INS-" + generadorIdInstitucion;
+	}
+	
+	public String generarIdSolicitud() {
+		return "SOL-" + generadorIdSolicitud;
+	}
+	
+	public void registrarPersona(Persona p) {
+		this.personas.add(p);
+	}
+	
+	public void registrarInstitucion(Institucion i) {
+		this.instituciones.add(i);
+	}
+	
+	public Usuario iniciarSesion(String usuario, String pass) {
+		Usuario actual = null;
+		int contador = 0;
+		boolean encontrado = false;
+		
+		while (!encontrado && contador < this.usuarios.size()) {
+			actual = this.usuarios.get(contador);
+			if (actual.getNombre().equalsIgnoreCase(usuario) && actual.getPassword().equalsIgnoreCase(pass))
+				encontrado = true;
+			
+			contador++;
+		}
+		
+		return actual;
+	}
+	
+	public ArrayList<SolicitudEmp> listarSolicitudesPorPersona(Persona p) {
+		ArrayList<SolicitudEmp> result = new ArrayList<SolicitudEmp>();
+		
+		for (Solicitud actual : this.solicitudes) {
+			if (actual instanceof SolicitudEmp) {
+				SolicitudEmp soli = (SolicitudEmp)actual;
+				if (soli.getPersona().getIdentificador().equalsIgnoreCase(p.getIdentificador())) 
+					result.add(soli);
+			}
+		}
+		
+		return result;
+	}
 }
