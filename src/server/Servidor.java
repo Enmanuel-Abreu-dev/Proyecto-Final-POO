74b83c1ad8c66b8ab2af9c8a2c4df53ed147544d
José Rafael <joserafaeltineog@gmail.com>
2026-08-03 14:12:08 -0400

    implementacion de fecha en archivos de backups

--- src/server/Servidor.java
+++ src/server/Servidor.java
@@ -8,13 +8,13 @@ import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Servidor extends Thread {
-
    public static void main(String[] args) {
       int puerto = 7000;
       ServerSocket soc = null;
-
       try {
          soc = new ServerSocket(puerto);
       } catch (IOException e) {
@@ -22,15 +22,12 @@ public class Servidor extends Thread {
          System.exit(1);
       }
       System.out.println("Server online on port: " + puerto);
-
       while (true) {
          try {
             Socket cliente = soc.accept();
             System.out.println("> Client connect " + cliente.getInetAddress() + " at " + LocalDate.now().toString());
-
             ObjectInputStream data = new ObjectInputStream(cliente.getInputStream());
             guardarArchivo(data);
-
             cliente.close();
             System.out.println("> Sesion termined");
             data.close();
@@ -44,9 +41,21 @@ public class Servidor extends Thread {
       try {
          File arch = new File("./backups/save.bin");
 
+         // Si ya existe un archivo previo, lo renombramos con la fecha/hora actual
+         if (arch.exists()) {
+            String fecha = LocalDateTime.now().format(
+                  DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
+            File respaldo = new File("./backups/save_" + fecha + ".bin");
+            boolean renombrado = arch.renameTo(respaldo);
+            if (renombrado) {
+               System.out.println("> Archivo anterior renombrado a: " + respaldo.getName());
+            } else {
+               System.out.println("> No se pudo renombrar el archivo anterior");
+            }
+         }
+
          ObjectOutputStream oi = new ObjectOutputStream(new FileOutputStream(arch));
          oi.writeObject(data.readObject());
-
          oi.close();
          System.out.println("> Data saved");
       } catch (IOException | ClassNotFoundException e) {
